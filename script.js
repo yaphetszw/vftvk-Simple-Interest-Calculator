a7820a65f69482f6b04a47baa3e82e415318490a
yaphetszw <69821214+yaphetszw@users.noreply.github.com>
2021-04-26 22:48:17 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){  
+    var deposit=document.getElementById('principal').value;
+    var rate= document.getElementById('rate').value;
+    var year=document.getElementsByTagName('select')[0].value;
+    document.getElementById('result1').innerHTML=deposit; 
+    document.getElementById('result2').innerHTML=rate.toString()+"%"; 
+    document.getElementById('result3').innerHTML=deposit*Number(year)*rate/100; 
+    document.getElementById('result4').innerHTML=2021+Number(year);    
+    document.getElementsByTagName('article')[0]. style.display="block";
 }
+
+function updateTextInput(val) {
+          document.getElementById('range').innerHTML=val; 
+        }
         
